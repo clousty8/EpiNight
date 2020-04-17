c2a5b51fd69b0e0f6cb9633e5eb2cf3ca96ef5d4
Armand MOUNSI <armand.mounsi@epita.fr>
2020-04-17 17:38:12 +0200

    personage_dans_sa_scene

---
+++ Test unity/New Unity Project/Assets/makeahud02-scripts/Healthbar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public Slider healthbar;
+    private PlayerHealth _playerHealth;
+
+    private void Start()
+    {
+        _playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    private void Update()
+    {
+        healthbar.value = _playerHealth.health;
+    }
+}
