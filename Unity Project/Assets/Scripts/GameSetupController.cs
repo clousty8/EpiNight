6eef9fc08ea081367d72c97c0da7e8f60bb86618
zengijon <58482921+zengijon@users.noreply.github.com>
2020-04-18 17:36:32 +0200

    Multi_semble_ok_mais_bug_visu_du_déplacement?

--- Unity Project/Assets/Scripts/GameSetupController.cs
+++ Unity Project/Assets/Scripts/GameSetupController.cs
@@ -19,16 +19,16 @@ public class GameSetupController : MonoBehaviour
 
     private void CreatePlayerMaster()
     {
-        Vector3 vector3 = new Vector3(-1829, 140, -186);
+        Vector3 vector3 = new Vector3(-1, 0, -1);
         Debug.Log("Creating Player");
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EPITAMaster"), vector3, Quaternion.identity);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EPITA"), vector3, Quaternion.identity);
     }
 
     private void CreatePlayer()
     {
-        Vector3 vector3 = new Vector3(-1829, 140, -96);
-        Debug.Log("Creating Player");
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EPITA"), vector3, Quaternion.identity);
+        Vector3 vector3 = new Vector3(-1, 0, -6);
+        Debug.Log("Creating Player Master");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EPITAMaster"), vector3, Quaternion.identity);
     }
 
     // Update is called once per frame
