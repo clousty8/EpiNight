e7ed7f01ce0353261c7f8828e7983e07f0d66010
zengijon <58482921+zengijon@users.noreply.github.com>
2020-04-20 22:58:02 +0200

    Animation_et_déplacment_objet_fonctionnel

--- Unity Project/Assets/Scripts/GameSetupController.cs
+++ Unity Project/Assets/Scripts/GameSetupController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
@@ -6,6 +7,8 @@ using System.IO;
 
 public class GameSetupController : MonoBehaviour
 {
+    public List<GameObject> GameObjects = new List<GameObject>();
+    public List<Camera> Cameras = new List<Camera>();
     // Start is called before the first frame update
     void Start()
     {
@@ -20,15 +23,24 @@ public class GameSetupController : MonoBehaviour
     private void CreatePlayerMaster()
     {
         Vector3 vector3 = new Vector3(-1, 0, -1);
-        Debug.Log("Creating Player");
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EPITA"), vector3, Quaternion.identity);
+        Debug.Log("Creating Player Master");
+        var test = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EPITA"), vector3, Quaternion.identity);
+
+        GameObjects.Add(test);
+        Camera cam = test.GetComponentInChildren<Camera>();
+        Cameras.Add(cam);
+        cam.name = "CameraMaster";
     }
 
     private void CreatePlayer()
     {
         Vector3 vector3 = new Vector3(-1, 0, -6);
-        Debug.Log("Creating Player Master");
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EPITA"), vector3, Quaternion.identity);
+        Debug.Log("Creating Player");
+        var test = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EPITA"), vector3, Quaternion.identity);
+        GameObjects.Add(test);
+        Camera cam = test.GetComponentInChildren<Camera>();
+        Cameras.Add(cam);
+        cam.name = "CameraOther";
     }
 
     // Update is called once per frame
