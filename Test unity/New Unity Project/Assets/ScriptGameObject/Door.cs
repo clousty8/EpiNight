1ba17511eca5adba9b1a2bc36bf85548ba759abb
Armand MOUNSI <armand.mounsi@epita.fr>
2020-04-20 17:28:21 +0200

    nouveau_test

---
+++ Test unity/New Unity Project/Assets/ScriptGameObject/Door.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCam;
+    private bool beingCarried = false;
+    private bool touched = false;
+    private Text Txtmessage;
+    private Animator Anim;
+    private bool hasPlayer;
+
+    private void Start()
+    {
+        Txtmessage = GameObject.Find("NewText").GetComponent<Text>();
+        Anim = GameObject.Find("Door").GetComponent<Animator>();
+        hasPlayer = false;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        if (dist <= 4f)
+        {
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+        if (hasPlayer)
+        {
+            Txtmessage.text = "This is a cube";
+        }
+        else
+        {
+            Txtmessage.text = "";
+        }
+    
+        // si on peut ramasser et qu'on appuie sur E = on porte l'objet
+        if (hasPlayer && Input.GetKey(KeyCode.E))
+        {
+            Anim.SetBool("Open",true);
+        }
+    }
+}
---
+++ Test unity/New Unity Project/Assets/ScriptGameObject/PickableObject.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+    
+public class PickableObject : MonoBehaviour
+    {
+        public Transform player;
+        public Transform playerCam;
+        public float throwForce = 10;
+        private bool hasPlayer = false;
+        private bool beingCarried = false;
+        private bool touched = false;
+        private Text Txtmessage;
+
+        private void Start()
+        {
+            Txtmessage = GameObject.Find("TextTest").GetComponent<Text>();
+        }
+
+        void Update()
+        {
+            // check distance entre objet et joueur
+            float dist = Vector3.Distance(gameObject.transform.position, player.position);
+    
+            // si - ou = 1.9 unitÃ©s de distance = on peut ramasser
+            if (dist <= 4f)
+            {
+                hasPlayer = true;
+            }
+            else
+            {
+                hasPlayer = false;
+            }
+	    if (hasPlayer)
+	    {
+		Txtmessage.text = "This is a cube";
+	    }
+	    else
+        {
+            Txtmessage.text = "";
+        }
+    
+            // si on peut ramasser et qu'on appuie sur E = on porte l'objet
+            if (hasPlayer && Input.GetKey(KeyCode.E))
+            {
+                GetComponent< Rigidbody>().isKinematic = true;
+                transform.parent = playerCam;
+                beingCarried = true;
+            }
+    
+            // Si on porte l'objet
+            if (beingCarried)
+            {
+                // si l'objet touche un mur / objet avec collider
+                if (touched)
+                {
+                    GetComponent< Rigidbody>().isKinematic = false;
+                    transform.parent = null;
+                    beingCarried = false;
+                    touched = false;
+                }
+    
+                // Clique gauche = on jette l'objet
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent< Rigidbody>().isKinematic = false;
+                    transform.parent = null;
+                    beingCarried = false;
+                    GetComponent< Rigidbody>().AddForce(playerCam.forward * throwForce);
+                }
+                // clique droit on pose l'objet
+                else if (Input.GetMouseButtonDown(1))
+                {
+                    GetComponent< Rigidbody>().isKinematic = false;
+                    transform.parent = null;
+                    beingCarried = false;
+                }
+            }
+        }
+    
+        // DÃ©tection de contact grace au collider is trigger
+        void OnTriggerEnter()
+        {
+            if (beingCarried)
+            {
+                touched = true;
+            }
+        }
+    }
+
